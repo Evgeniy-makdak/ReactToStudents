6c7fbd9777e3ceb780876db365e4130a56c445b5
Evgeniy-makdak <makdak23@mail.ru>
2025-06-02 14:00:21 +0300

    Переписал компоненты App и Boock через JSX

--- RealReactToStudents/realreacttostudents/src/index.js
+++ RealReactToStudents/realreacttostudents/src/index.js
@@ -1,9 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+/* eslint-disable no-unreachable */
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const Book = (props) => {
+  return <div>
+    <h2>{ props.name }</h2>
+    <p>{ props.year }</p>
+    <p>{ props.price }</p>
+  </div>
+  // React.createElement("div", {}, [
+  //   React.createElement("h2", {}, props.name),
+  //   React.createElement("p", {}, props.year),
+  //   React.createElement("p", {}, props.price),
+  // ]);
+};
+
+const App = () => {
+  return (
+    <div>
+      <h1>"Создаём приложение на React"</h1>
+      <Book name="JS" year="2019" price="$87" />
+      <Book name="JS for beginners" year="2016" price="$33" />
+      <Book name="React" year="2025" price="$117" />
+    </div>
+  );
+
+  // React.createElement("div", {}, [
+  //   React.createElement("h1", {}, "Создаём приложение на React"),
+  //   React.createElement(Book, { name: "JS", year: 2019, price: "$87" }),
+  //   React.createElement(Book, {
+  //     name: "JS for beginners",
+  //     year: 2016,
+  //     price: "$33",
+  //   }),
+  //   React.createElement(Book, { name: "React", year: 2025, price: "$117" }),
+  // ]);
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
